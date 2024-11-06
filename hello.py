c986f4ee796f286c1e960ee2a0d01a372e686120
NotAdan17 <30250580@blackpool.ac.uk>
2024-11-06 10:00:18 +0000

    Update hello.py
    Run time error solved with comments

--- hello.py
+++ hello.py
@@ -1,14 +1,16 @@
-# Python program to calculate the area of a rectangle
-
 def calculate_area(length, width):
+    # Use multiplication for calculating area
     area = length * width
     return area
 
-length = input("Enter the length of the rectangle: ")
-width = input("Enter the width of the rectangle: ")
+try:
+    # Get user inputs and convert to integers
+    length = int(input("Enter the length of the rectangle: "))
+    width = int(input("Enter the width of the rectangle: "))
+    
+    # Call function and print area
+    print("The area of the rectangle is:", calculate_area(length, width))
 
-length = int(length)
-width = int(width)
-
-print("The area of the rectangle is:", calculate_area(length, width))
-
+except ValueError:
+    # Handle non-numeric input
+    print("Please enter valid numbers for length and width.")
